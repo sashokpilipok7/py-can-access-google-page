de1b8004d8800e81c03d19e382eb6766a1d4313d
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-07-01 01:39:08 +0300

    feat: Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+import time
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+def test_can_access_google_page() -> None:
+    current_time = time.strftime("%H")
+    if current_time in range(6, 23):
+        assert can_access_google_page("https://data.rocks") == "Accessible"
+    else:
+        assert can_access_google_page("https://data.rocks") == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_with_functions(
+        mocked_has_connection: dict,
+        mocked_valid_url: dict) -> None:
+    mocked_valid_url.return_value = True
+    mocked_has_connection.return_value = True
+    assert can_access_google_page("https://data.rocks") == "Accessible"
+
+    mocked_valid_url.assert_called()
+    mocked_has_connection.assert_called()
